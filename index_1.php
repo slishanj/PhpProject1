ee1c9834600e136de0a9c1368694a02226eb5598
ishan sam <slishan@gmail,com>
2013-06-21 22:54:27 +0530

    langClasses_kurg

---
+++ index_1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $n="shan";
+        $p=123;
+        
+        if($_GET['name']==$n && $_GET['pass']==$p){
+            echo 'Helloooo';
+        }else{
+            echo 'ndngkdfnkgdkfkgjdfnkjdfkgj....';
+        };[]
+        ?>
+    </body>
+</html>
